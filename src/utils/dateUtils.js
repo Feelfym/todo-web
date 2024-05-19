6a083e66589642c5575d73490a10ab182e19d64f
Feelfym <tomoya.noguchi@gmail.com>
2024-05-19 19:01:37 +0900

    リファクタリング

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,8 +1,8 @@
+import { format } from 'date-fns';
+
 export const addOneDay = (date) => {
   const result = new Date(date);
   result.setDate(result.getDate() + 1);
-  const year = result.getFullYear();
-  const month = String(result.getMonth() + 1).padStart(2, '0');
-  const day = String(result.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
+  return format(result, 'yyyy-MM-dd');
 };
+
